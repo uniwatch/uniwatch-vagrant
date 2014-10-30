cdf45ebc5a2bfc79e8cfc13ce52099b3608871fd
Maksim Dudkin <maksim.dudkin@mtechlab.net>
2014-10-31 01:51:52 +0200

    Some changes

--- ansible/db/init.sql
+++ ansible/db/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE uniwatch (UTF-8);
+CREATE DATABASE `uniwatch` CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER uniwatch@localhost identified by '1q2w3e';
 GRANT ALL ON uniwatch.* TO uniwatch@localhost;
 FLUSH privileges;
